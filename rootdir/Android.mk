ddf5c9eada40e614b8b076fc9dc29a40a09798f1
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2021-07-27 02:23:59 +0000

    RMX2185: rootdir: Update zram scripts based on available ram
    * Picked from stock rom
    https://github.com/MinatiScape/realme_rmx2185_dump/blob/RMX2185_11.A.93_0930_202104192215/system/system/etc/swap_enable.sh

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -39,3 +39,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.safailnet.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := set_zram.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES    := bin/set_zram.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+include $(BUILD_PREBUILT)
